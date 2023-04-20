fa8a0ebffb5b4b5afe310a9613e000caabb8264e
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-04-20 18:17:18 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,8 +1,44 @@
 package de.demmer.dennis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Application {
+
 	public static void main(String[] args) {
 		
+		
+		String s5 = "Test";
+		System.out.println(s5.equals("Test"));
+		Date date = new Date();
+		
+		
+
+		InputStreamReader ir = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(ir);
+
+//		try {
+//		  String s = br.readLine();
+//		  System.out.println("BufferedReader: " + s);
+//		} catch(IOException ioe) {
+//			ioe.printStackTrace();
+//		}
+
+		// ------------------------------
+
+//		
+//		Scanner scanner = new Scanner(System.in);
+//
+//		while (true) {
+//
+//			String s1 = scanner.nextLine();
+//			System.out.println("Scanner: " + s1);
+//
+//		}
+
 	}
 
 }
